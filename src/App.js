ef4091f626eda3a9fb09a3d9b4ee10dc5aedb98c
lavaspoon <lavaspoon@gmail.com>
2024-08-07 00:14:12 +0900

    여기까지

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './component/Home';
 import NewPage from './component/NewPage';
+import ChatResponse from './component/ChatResponse';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/:text" element={<NewPage />} /> {/* URL 경로에서 파라미터를 받습니다. */}
+        <Route path="/:text" element={<NewPage />} />
+        <Route path="/codeblock" element={<ChatResponse />} />
       </Routes>
     </Router>
   );
